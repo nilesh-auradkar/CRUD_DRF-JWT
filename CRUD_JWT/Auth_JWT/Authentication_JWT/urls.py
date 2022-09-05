7b89c1af18d8cd344cdcb9f394c426bce1bbbe6b
nilesh-auradkar <nilesh.auradkar14@gmail.com>
2022-09-06 00:11:34 +0530

    1. Fixed Django secret key. 2. Added JWT token authentication. 3. Added features for Change Password, Send Password Reset Link to user email, Reset password through link. 4. Added Postman Endpoints file.

--- CRUD_JWT/Auth_JWT/Authentication_JWT/urls.py
+++ CRUD_JWT/Auth_JWT/Authentication_JWT/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path, include
-from Authentication_JWT.views import UserRegistrationView, UserLoginView
+from Authentication_JWT.views import (UserRegistrationView,
+                                      UserLoginView,
+                                      UserProfileView,
+                                      UserChangePasswordView, 
+                                      SendPasswordResetEmailView,
+                                      UserPasswordResetView)
 
 urlpatterns = [
     path("register/", UserRegistrationView.as_view(), name="register"),
     path("login/", UserLoginView.as_view(), name="login"),
+    path("profile/", UserProfileView.as_view(), name="profile"),
+    path("changepassword/", UserChangePasswordView.as_view(), name="changepassword"),
+    path("send-reset-password/", SendPasswordResetEmailView.as_view(), name="send-reset-password"),
+    path("reset-password/<uid>/<token>/", UserPasswordResetView.as_view(), name="reset-password"),
 ]
